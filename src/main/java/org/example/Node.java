cda5433cdda90f3af54d6855da7bc706af80460a
dreik84 <dreik84@mail.ru>
2025-10-12 16:09:33 +0300

    Update Node.java

--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -5,8 +5,8 @@ public class Node {
     private static final int ORDER = 4;
     private int numItems;
     private Node parent;
-    private Node[] childArray = new Node[ORDER];
-    private DataItem[] itemArray = new DataItem[ORDER - 1];
+    private final Node[] childArray = new Node[ORDER];
+    private final DataItem[] itemArray = new DataItem[ORDER - 1];
 
     public void connectChild(int childNum, Node child) {
         childArray[childNum] = child;
@@ -41,5 +41,49 @@ public class Node {
 
     public boolean isFull() {
         return numItems == ORDER - 1;
+    }
+
+    public int findItem(long key) {
+
+        for (int i = 0; i < ORDER - 1; i++) {
+            if (itemArray[i] == null) break;
+            else if (itemArray[i].dData == key) return i;
+        }
+        return -1;
+    }
+
+    public int insertItem(DataItem newItem) {
+        numItems++;
+        long newKey = newItem.dData;
+
+        for (int i = ORDER - 2; i >= 0; i--) {
+            if (itemArray[i] == null) {
+                continue;
+            } else {
+                long itsKey = itemArray[i].dData;
+
+                if (newKey < itsKey) {
+                    itemArray[i + 1] = itemArray[i];
+                } else {
+                    itemArray[i + 1] = newItem;
+                    return i + 1;
+                }
+            }
+        }
+
+        itemArray[0] = newItem;
+        return 0;
+    }
+
+    public DataItem removeItem() {
+        DataItem temp = itemArray[numItems - 1];
+        itemArray[numItems - 1] = null;
+        numItems--;
+        return temp;
+    }
+
+    public void displayNode() {
+        for (int i = 0; i < numItems; i++) itemArray[i].displayItem();
+        System.out.println("/");
     }
 }
